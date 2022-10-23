1a88b98cc8761388c983facc7252b4f06e571744
Archer <820239854@qq.com>
2022-10-23 11:24:48 +0800

    成功渲染窗口

--- src/main.c
+++ src/main.c
@@ -1,12 +1,91 @@
+#include <windows.h>
+
 #include<stdio.h>
+#include<stdbool.h>
 #include<SDL.h>
 
-//bool initialize_window(void)
-//{
-//	SDL_Init(SDL_INIT_EVERYTHING);
-//}
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 
-int main() {
-	//initialize_window();
+bool initialize_window(void)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		fprintf(stderr, "Error init SDL");
+		return false;
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		600,
+		SDL_WINDOW_BORDERLESS
+	);
+
+	if (!window)
+	{
+		if (!window)
+			fprintf(stderr, "Error init window");
+		return false;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer)
+	{
+		if (!window)
+			fprintf(stderr, "Error init renderer");
+		return false;
+	}
+
+	return true;
+}
+
+void setup(void)
+{
+
+}
+
+void process_input(void)
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type)
+	{
+	case SDL_QUIT:
+		is_running = false;
+		break;
+	case SDL_KEYDOWN:
+		if (event.key.keysym.sym == SDLK_ESCAPE)
+			is_running = false;
+		break;
+	}
+}
+
+void update(void)
+{
+
+}
+
+void render(void)
+{
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	is_running = initialize_window();
+
+	setup();
+	while (is_running)
+	{
+		process_input();
+		update();
+		render();
+	}
 	return 0;
 }
