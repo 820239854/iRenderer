8683648e45866d213a9f400eb9b3a3c9e26e6311
Archer <820239854@qq.com>
2022-10-23 12:06:22 +0800

    渲染ColorBuffer

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include <windows.h>
 
 #include<stdio.h>
+#include<stdint.h>
 #include<stdbool.h>
 #include<SDL.h>
 
 bool is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void)
 {
@@ -19,8 +25,8 @@ bool initialize_window(void)
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
+		window_width,
+		window_height,
 		SDL_WINDOW_BORDERLESS
 	);
 
@@ -44,7 +50,10 @@ bool initialize_window(void)
 
 void setup(void)
 {
+	color_buffer = (uint32_t*)malloc(window_width*window_height*sizeof(uint32_t));
 
+	color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+	                                         SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
 }
 
 void process_input(void)
@@ -68,12 +77,40 @@ void update(void)
 
 }
 
+void clear_color_buffer(uint32_t color)
+{
+	for(int y=0; y<window_height; y++)
+	{
+		for(int x=0; x<window_width; x++)
+		{
+			color_buffer[y*window_width + x] = color;
+		}
+	}
+}
+
+void render_color_buffer(void)
+{
+	SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, (int)(sizeof(uint32_t)*window_width));
+	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
 void render(void)
 {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
+	render_color_buffer();
+	clear_color_buffer(0xFFFF00);
+
 	SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void)
+{
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -87,5 +124,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		update();
 		render();
 	}
+	destroy_window();
 	return 0;
 }
