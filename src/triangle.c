0248a6efaeb279fe3c2299e31658dcba7a7763b7
Archer <820239854@qq.com>
2022-11-28 11:37:09 +0800

    avoid divided by zero

--- src/triangle.c
+++ src/triangle.c
@@ -53,8 +53,19 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
 		int_swap(&y0, &y1);
 		int_swap(&x0, &x1);
 	}
-	int My = y1;
-	int Mx = ((float)((x2 - x0) * (y1-y0)) / (float)(y2-y0)) + x0;
-	fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
-	fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+	if(y1 == y2)
+	{
+		fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+	}
+	else if (y0 == y1)
+	{
+		fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+	}
+	else
+	{
+		int My = y1;
+		int Mx = ((float)((x2 - x0) * (y1-y0)) / (float)(y2-y0)) + x0;
+		fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+		fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+	}
 }
