9c9025d06d59d558ffd5d08010d84348bc3aa991
Archer <820239854@qq.com>
2022-11-28 16:04:18 +0800

    画家算法

--- src/main.c
+++ src/main.c
@@ -69,7 +69,6 @@ void update(void)
 		SDL_Delay(time_to_wait);
 	}
 	previoous_frame_time = SDL_GetTicks();
-
 	triangles_to_render = NULL;
 
 	mesh.rotation.x += 0.01;
@@ -122,6 +121,7 @@ void update(void)
 			projected_points[j].x += (window_width / 2);
 			projected_points[j].y += (window_height / 2);
 		}
+		float avg_depth = (transformed_vertices[0].z + transformed_vertices[1].z + transformed_vertices[2].z) / 3.0;
 		triangle_t projected_triangle = {
 			.points =
 			{
@@ -129,9 +129,24 @@ void update(void)
 				{ projected_points[1].x, projected_points[1].y },
 				{ projected_points[2].x, projected_points[2].y },
 			},
-			.color = mesh_face.color
+			.color = mesh_face.color,
+			.avg_depth = avg_depth
 		};
 		array_push(triangles_to_render, projected_triangle);
+
+		int num_triangles = array_length(triangles_to_render);
+		for (int i = 0; i < num_triangles; i++)
+		{
+			for (int j = i; j < num_triangles; j++)
+			{
+				if(triangles_to_render[i].avg_depth < triangles_to_render[j].avg_depth)
+				{
+					triangle_t tmp = triangles_to_render[i];
+					triangles_to_render[i] = triangles_to_render[j];
+					triangles_to_render[j] = tmp;
+				}
+			}
+		}
 	}
 }
 
